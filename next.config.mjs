dbd7f08d418a9a0e15113dda27e848dbf4de5800
AddinRizal <addinhadi10403@gmail.com>
2025-07-15 15:12:46 +0700

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
+        port: "",
+        pathname: "/vi/**",
+      },
+    ],
+  },
+};
+
+export default withNextIntl(nextConfig);
